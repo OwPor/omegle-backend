efa0c393e1b7e0eef5d11d04dabbbccda6b7623a
OwPor <rayvincentconcepcion@gmail.com>
2024-12-23 21:30:53 +0800

    with vercel

--- app.js
+++ app.js
@@ -10,20 +10,21 @@ const server = createServer(app)
 
 app.use(express.json())
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://owmegle.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }))
 
 app.get("/", (req, res) => {
-    res.send("Hello this is test message")
+    res.redirect(301, "https://owmegle.vercel.app/")
 })
 
 const port = process.env.PORT || 5000
 
 server.listen(port, () => {                                                                                         
-    console.log(`Server run on port http://localhost:${port}`)
+    // console.log(`Server run on port http://localhost:${port}`)
+    console.log(`Server running`)
 })
 
 socketSetup(server)
